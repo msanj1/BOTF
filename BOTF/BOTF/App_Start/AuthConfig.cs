ca4fec78809a079063fc46bbeba45de601899574
msanj1 <mohsen@wolfbyte.com.au>
2012-11-10 20:02:23 +1000

    v 1.0
    original version of BOTF

---
+++ BOTF/BOTF/App_Start/AuthConfig.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Web.WebPages.OAuth;
+using BOTF.Models;
+
+namespace BOTF
+{
+    public class userService
+    {
+        public string Token { get; set; }
+        public string Provider { get; set; }
+    }
+
+    public static class AuthConfig
+    {
+        public static void RegisterAuth()
+        {
+            
+         
+
+            /*Tweeter Configuration*/
+            Settings.Settings.TwitterConsumerKey = "LKLeLT9sw38CdVTnWb8IIw";
+            Settings.Settings.TwitterConsumerSecret = "uKr3F9GKjNkBGuE7DCbZvR4xcRcsPPU1ibLpPg0U";
+
+            /*Facebook Configuration*/
+            Settings.Settings.FacebookAppId = "413005845422605";
+            Settings.Settings.FacebookAppSecret = "f8afed946bc4c981cc70908f4c9a1a56";
+            
+
+            /*Facebook/Twitter callback Configuration*/
+            Settings.Settings.TwitterCallbackURL = "http://botf.azurewebsites.net/Account/TwitterCallback";
+            Settings.Settings.FacebookCallbackURL = "http://botf.azurewebsites.net/Account/FBAuth";
+
+            /*LastFM Configuration*/
+            Settings.Settings.LastFMKey = "637768bd3e08f631449bea2f1e28bf72";
+           
+
+            //OAuthWebSecurity.RegisterMicrosoftClient(
+            //    clientId: "",
+            //    clientSecret: "");
+
+            OAuthWebSecurity.RegisterTwitterClient(
+                consumerKey: Settings.Settings.TwitterConsumerKey,
+                consumerSecret: Settings.Settings.TwitterConsumerSecret);
+
+
+            OAuthWebSecurity.RegisterFacebookClient(
+                appId: Settings.Settings.FacebookAppId,
+                appSecret: Settings.Settings.FacebookAppSecret
+               
+             );
+             
+           
+
+            //OAuthWebSecurity.RegisterGoogleClient();
+        }
+    }
+}
---
+++ BOTF/BOTF/Models/LastFM.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Net;
+using System.Xml.Linq;
+using System.IO;
+using System.Xml;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace BOTF.Models
+{
+    public class LastFM
+    {
+
+     string apiKey;
+     string host;
+        public LastFM(string API, string Host )
+        {
+             apiKey = API;
+             host = Host;
+        
+        }
+
+        class SampleData
+        {
+            [JsonProperty(PropertyName = "items")]
+            public System.Data.DataTable Items { get; set; }
+        }
+
+        public List<string> SearchVenues(string Venue)
+        {
+            string URL = host + "?method=venue.search&api_key=" + apiKey + "&venue=" + Venue + "&limit=5";
+            var client = new WebClient();
+            string xml = client.DownloadString(URL);
+            List<string> tags = new List<string>();
+            var xdoc = XDocument.Load(new StringReader(xml));
+            List<string> venues = xdoc.Descendants("results").Elements("venuematches").Elements("venue").Elements("name").Select(c=>c.Value).ToList();
+            return venues;
+        }
+
+        public List<string> GetSimilarArtists(string artistName) 
+        {
+            string URL = host + "?method=artist.getinfo&artist=" + artistName + "&api_key=" + apiKey + "&autocorrect=1";
+            var client = new WebClient();
+            string xml = client.DownloadString(URL);
+            List<string> tags = new List<string>();
+            var xdoc = XDocument.Load(new StringReader(xml));
+            var names = xdoc.Descendants("artist").Elements("similar").Elements("artist").Elements("name").Select(r => r.Value).Take(5).ToList();
+            return names;
+        }
+
+        public dynamic GetArtistInfo( string artistName)
+        {
+          
+            string URL = host + "?method=artist.getinfo&artist=" + artistName + "&api_key=" + apiKey + "&autocorrect=1";
+            var client = new WebClient();
+            string xml = client.DownloadString(URL);
+
+
+            
+            List<string> tags = new List<string>();
+
+            var xdoc = XDocument.Load(new StringReader(xml));
+            var data = from item in xdoc.Descendants("artist")
+                       select new
+                       {
+                           artist = item.Element("name").Value,
+                           image = item.Element("image").Attribute("small").Value,
+                       };
+            var correctname = xdoc.Descendants("artist").Elements("name").Select(r => r.Value).ToArray();
+            var tagdata = xdoc.Descendants("artist").Elements("tags").Elements("tag").Elements("name").Select(r => r.Value).ToArray();
+            var imgs = xdoc.Descendants("artist").Elements("image")
+               .Where(node => (string)node.Attribute("size") == "mega")
+               .Select(node => node.Value.ToString())
+               .ToList();
+            var artistbio = xdoc.Descendants("artist").Elements("bio").Elements("summary").Select(r => r.Value).Take(1).ToArray();
+            var genre = xdoc.Descendants("artist").Elements("tags").Elements("tag").Elements("name").Select(r => r.Value).Take(1).ToArray();
+            string image = "";
+            string bio = "";
+            string Genre = "";
+
+           
+           
+            image = imgs[0];
+            bio = artistbio[0];
+            Genre = genre[0];
+
+            return new { PictureURL = image, ArtistBio = bio, MusicGenre = Genre, ArtistName = correctname[0] };
+        }
+
+        public List<string> SearchArtist(string artist)
+        {
+            string url = host + "?method=artist.search&artist=" + artist + "&api_key=" + apiKey + "&limit=5";
+            var client = new WebClient();
+            string xml = client.DownloadString(url);
+
+            List<string> names = new List<string>();
+            XmlTextReader reader = new XmlTextReader(new StringReader(xml));
+            reader.MoveToContent();
+            while (reader.Read())
+            {
+                if (reader.NodeType == XmlNodeType.Element)
+                {
+                    if (reader.Name == "name")
+                    {
+                        XElement el = XNode.ReadFrom(reader) as XElement;
+                        if (el != null)
+                        {
+                            names.Add(el.Value);
+                        }
+                    }
+                }
+            }
+            return names;
+        }
+    }
+}
